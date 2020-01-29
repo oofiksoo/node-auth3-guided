8c8dc80013ee3a5c66cafe742241302537edeeef
Joseph Graham <Joseph@Chasingthefiks.net>
2020-01-29 12:12:35 -0700

    token distributed, and consumed

--- users/users-router.js
+++ users/users-router.js
@@ -1,14 +1,23 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Users = require('./users-model.js');
-const restricted = require('../auth/restricted-middleware.js');
+const Users = require("./users-model.js");
+const restricted = require("../auth/restricted-middleware.js");
 
-router.get('/', restricted, (req, res) => {
-  Users.find()
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
+router.get("/", restricted, onlyhouse("test"), (req, res) => {
+    Users.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
 });
 
+function onlyhouse(house) {
+    return function(req, res, next) {
+        if (req.user && req.user && req.user.house.toLowerCase() === house) {
+            next();
+        } else {
+            res.status(403).json({ you: "have no power here" });
+        }
+    };
+}
 module.exports = router;
